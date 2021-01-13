3bde61df2266ea43f3f89c6d4d9a93b42aad512d
Jovert Palonpon <jovertical@gmail.com>
2021-01-13 10:03:22 +0800

    chore: Untrack mix assets + remove tests page

--- resources/js/Pages/Tests/Index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import App from '~/Shared/Layouts/App';
-</script>
-
-<App title="Tests">
-    <div class="py-4">
-        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-    </div>
-</App>
